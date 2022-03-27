b51c32831c59ae450046d642980699aa8fddc181
forest1040 <forest1040@gmail.com>
2022-03-28 06:59:04 +0900

    量子状態を移植

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//use num::complex::Complex;
+use num::complex::Complex;
 use qustV2::state::QuantumState;
 
 // fn set(sim: &mut QuantumSimulator, qubit: &Qubit, r: MeasuredResult) {
@@ -40,4 +40,47 @@ fn main() {
     let seed = 0;
     state.set_haar_random_state(seed);
     println!("{}", state);
+
+    state.set_zero_state();
+    let vec = state.get_vector();
+    println!("{:?}", vec);
+
+    let mut new_state = Vec::new();
+    new_state.push(Complex { re: 0.5, im: 0. });
+    new_state.push(Complex { re: 0.5, im: 0. });
+    new_state.push(Complex { re: 0.5, im: 0. });
+    new_state.push(Complex { re: 0.5, im: 0. });
+
+    state.load(new_state);
+    println!("{}", state);
+
+    let n = 5;
+    let mut state = QuantumState::new(n);
+    state.set_haar_random_state(seed);
+    let index = 3;
+    let zero_probability = state.get_zero_probability(index);
+    println!("prob_meas_3rd : {}", zero_probability);
+
+    let n = 2;
+    let mut state = QuantumState::new(n);
+    let mut new_state = Vec::new();
+    new_state.push(Complex {
+        re: 1. / 2.0_f64.sqrt(),
+        im: 0.,
+    });
+    new_state.push(Complex { re: 0., im: 0. });
+    new_state.push(Complex { re: 0.5, im: 0. });
+    new_state.push(Complex { re: 0.5, im: 0. });
+    state.load(new_state);
+    let data = state.sampling(10);
+    println!("{:?}", data);
+
+    // 内積値の計算
+    let n = 5;
+    let mut state_bra = QuantumState::new(n);
+    let mut state_ket = QuantumState::new(n);
+    state_bra.set_haar_random_state(0);
+    state_ket.set_computational_basis(0);
+    let value = QuantumState::inner_product(state_bra, state_ket);
+    println!("{:?}", value);
 }
--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,5 @@
 use num::complex::Complex;
-use rand::distributions::Standard;
+use rand::distributions::{Standard, WeightedIndex};
 //use rand::distributions::StandardNormal;
 use rand::prelude::*;
 use std::fmt;
@@ -13,7 +13,7 @@ pub struct QuantumState {
 
 impl QuantumState {
     pub fn new(n: usize) -> QuantumState {
-        let dim = 1 << n;
+        let dim = 1usize << n;
         let mut states = vec![Complex::new(0., 0.); dim];
         states[0] = Complex::new(1., 0.);
         QuantumState {
@@ -65,6 +65,76 @@ impl QuantumState {
             self.states[i] /= norm;
         }
     }
+
+    pub fn get_vector(&self) -> &Vec<Complex<f64>> {
+        &self.states
+    }
+
+    pub fn get_dim(&self) -> usize {
+        self.dim
+    }
+
+    pub fn load(&mut self, states: Vec<Complex<f64>>) {
+        // TODO: validate
+        // if (_state.size() != _dim) {
+        //     throw std::invalid_argument("inconsistent dim");
+        // }
+        self.states = states
+    }
+
+    pub fn get_zero_probability(&self, target_qubit_index: usize) -> f64 {
+        // TODO: validate
+        // if (target_qubit_index >= this->qubit_count) {
+        //     throw std::invalid_argument("qubit index >= num_qubit");
+        // }
+        m0_prob(target_qubit_index, &self.get_vector(), self.dim)
+        // let mask = 1usize << target_qubit_index;
+        // let mut sum = 0.;
+        // for i in 0..self.dim << 1 {
+        //     let temp_basis = (i >> target_qubit_index) << (target_qubit_index + 1);
+        //     let basis_1 = (temp_basis + i % mask) ^ mask;
+        //     sum += self.states[basis_1].powi(2).re;
+        // }
+        // sum
+    }
+
+    pub fn sampling(&self, sampling_count: usize) -> Vec<usize> {
+        let mut sum = 0.;
+        let mut weights = Vec::with_capacity(self.dim + 1);
+        weights.push(0.);
+        for i in 0..self.dim {
+            sum += self.states[i].norm();
+            weights.push(sum);
+        }
+        let dist = WeightedIndex::new(&weights).unwrap();
+        let mut rng = thread_rng();
+        let mut result: Vec<usize> = Vec::with_capacity(sampling_count);
+        for _ in 0..sampling_count {
+            result.push(dist.sample(&mut rng) - 1);
+        }
+        result
+    }
+
+    pub fn inner_product(state_bra: QuantumState, state_ket: QuantumState) -> Complex<f64> {
+        // double real_sum = 0.;
+        // double imag_sum = 0.;
+        // ITYPE index;
+        // for (index = 0; index < dim; ++index) {
+        //     CTYPE value;
+        //     value += conj(state_bra[index]) * state_ket[index];
+        //     real_sum += _creal(value);
+        //     imag_sum += _cimag(value);
+        // }
+        // return real_sum + 1.i * imag_sum;
+        let mut real_sum = 0.;
+        let mut imag_sum = 0.;
+        for i in 0..state_bra.dim {
+            let value = state_bra.states[i].conj() * state_ket.states[i];
+            real_sum += value.re;
+            imag_sum += value.im;
+        }
+        Complex::new(real_sum, imag_sum)
+    }
 }
 
 impl fmt::Display for QuantumState {
@@ -81,4 +151,23 @@ impl fmt::Display for QuantumState {
         );
         write!(f, "{}", tmp)
     }
+}
+
+fn m0_prob(target_qubit_index: usize, state: &Vec<Complex<f64>>, dim: usize) -> f64 {
+    let mask = 1usize << target_qubit_index;
+    let mut sum = 0.;
+    for i in 0..dim >> 1 {
+        let basis_1 = insert_zero_to_basis_index(i, mask, target_qubit_index) ^ mask;
+        //sum += pow(_cabs(state[basis_1]), 2);
+        sum += state[basis_1].l1_norm().powi(2);
+    }
+    sum
+}
+
+#[inline]
+fn insert_zero_to_basis_index(basis_index: usize, basis_mask: usize, qubit_index: usize) -> usize {
+    // ITYPE temp_basis = (basis_index >> qubit_index) << (qubit_index + 1);
+    // return temp_basis + basis_index % basis_mask;
+    let temp_basis = (basis_index >> qubit_index) << (qubit_index + 1);
+    temp_basis + basis_index % basis_mask
 }
